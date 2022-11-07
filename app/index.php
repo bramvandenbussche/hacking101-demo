12c877f071d84b1a3c5d55900f1b5b13c921f3c2
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-11-07 17:55:58 +0100

    Basic layouting

--- app/index.php
+++ app/index.php
@@ -6,31 +6,44 @@
     </head>
 
     <body>
+        <div class="container">
 
-        <center>
-            <h1>All Secure</h1>
-            <small>A very secure blog</small>
-        </center>
+            <div class="jumbotron">
+                <h1>All Secure!</h1>
+                <p>A very secure blog</p>
+                <p><a class="btn btn-primary btn-lg" href="https://www.infinitaslearning.com/" traget="_blank" role="button">Learn more</a></p>
+            </div>
+            <center>
+                <h1>All Secure</h1>
+                
+            </center>
 
-        <section>
-            <?php
-                // Get articles and print them
-                $sql = "SELECT id, title, thumbnail FROM articles WHERE id > 0";
-                
-                $result = $db_conn->query($sql);
-                
-                if ($result->num_rows > 0) {
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-            ?>
-                <article>
-                    <h1><?= $row['title'] ?></h1>
-                    <img src="/assets/thumbnails/<?= $row['thumbnail'] ?>" alt="" width="200" />
-                </article>
-            <?php
+            <div class="row">                
+                <?php
+                    // Get articles and print them
+                    $sql = "SELECT id, title, excerpt, thumbnail FROM articles WHERE id > 0";
+                    
+                    $result = $db_conn->query($sql);
+                    
+                    if ($result->num_rows > 0) {
+                        // output data of each row
+                        while($row = $result->fetch_assoc()) {
+                ?>
+                    <div class="col-sm-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="/assets/thumbnails/<?= $row['thumbnail'] ?>" class="card-img-top" alt="<?= $row['title'] ?>">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $row['title'] ?></h5>
+                                <p class="card-text"><?= $row['excerpt'] ?></p>
+                                <a href="/article.php?id=<?= $row['id'] ?>">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                        }
                     }
-                }
-            ?>
-        </section>
+                ?>
+            </div>
+        </div>
     </body>
 </html>
