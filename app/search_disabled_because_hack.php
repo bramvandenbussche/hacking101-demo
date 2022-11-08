971a1e1352721eb885e0e293a6fe70d8d559b2a7
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-11-08 15:11:58 +0100

    Make it strappy!

--- app/search_disabled_because_hack.php
+++ app/search_disabled_because_hack.php
@@ -1,38 +1,41 @@
 <?php require ("includes/database.inc.php"); ?>
 <html>
-<head>
-    <title>Search Galore!</title>
-    <?php include('includes/head.inc.php') ?>
-</head>
+    <head>
+        <title>Search Galore!</title>
+        <?php include('includes/head.inc.php') ?>
+    </head>
 
-<body>
+    <body>
+        <div class="container">
 
-<h3>The most advanced search thingy in the world!</h3>
+            <?php include('includes/menu.inc.php') ?>
 
+            <h3>The most advanced search thing in the world!</h3>
 
-<form method="get">
-<?php
-$theValue = "Search Me!";
-if (isset($_GET['search'])) {
-    $theValue = $_GET['search'];
-}
-?>
+            <form method="get">
+            <?php
+            $theValue = "Search Me!";
+            if (isset($_GET['search'])) {
+                $theValue = $_GET['search'];
+            }
+            ?>
 
-    <input type="text" name="search" value="<?= $theValue; ?>" />
-    <input type="submit" value="Find it!">
-</form>
+                <input type="text" name="search" value="<?= $theValue; ?>" />
+                <input type="submit" value="Find it!">
+            </form>
 
-<?php
-if (isset($_GET['search'])) {
-    echo '<table><th>Title</th><th>Article</th>';
-    $results = DbSearch($db_conn, $theValue);
-    while($row = $results->fetch_assoc()) {
+            <?php
+            if (isset($_GET['search'])) {
+                echo '<table><th>Title</th><th>Article</th>';
+                $results = DbSearch($db_conn, $theValue);
+                while($row = $results->fetch_assoc()) {
 
-        echo "<tr><td>".$row['title']."</td><td>".$row['article']."</td></tr>";
+                    echo "<tr><td>".$row['title']."</td><td>".$row['article']."</td></tr>";
 
-    }
-    echo '</table>';
-}
-?>
-</body>
+                }
+                echo '</table>';
+            }
+            ?>
+        </div>
+    </body>
 </html>
