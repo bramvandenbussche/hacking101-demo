0f5e83901d274be5027a1d8ab35ab50f07ffc965
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-11-08 14:01:32 +0100

    Add more content

--- sql/init.sql
+++ sql/init.sql
@@ -77,11 +77,81 @@ INSERT INTO articles (id, title, thumbnail, excerpt, article) VALUES
         <div>
             <div>
                 <p cms-metadescription>
-                    SQL injection (SQLi) is a web security vulnerability that allows an attacker to interfere with the queries that an application makes to its database. It generally allows an attacker to view data that they are not normally able to retrieve. This might include data belonging to other users, or any other data that the application itself is able to access. In many cases, an attacker can modify or delete this data, causing persistent changes to the application's content or behavior.
+                    SQL injection (SQLi) is a web security vulnerability that allows an attacker to interfere with the queries that an application makes to its database.
+                    It generally allows an attacker to view data that they are not normally able to retrieve. This might include data belonging to other users, 
+                    or any other data that the application itself is able to access. In many cases, an attacker can modify or delete this data, 
+                    causing persistent changes to the application's content or behavior.
                 </p>
                 <p>
-                    In some situations, an attacker can escalate an SQL injection attack to compromise the underlying server or other back-end infrastructure, or perform a denial-of-service attack.
+                    In some situations, an attacker can escalate an SQL injection attack to compromise the underlying server or other back-end infrastructure, 
+                    or perform a denial-of-service attack.
                 </p>
             </div>
         </div>
+        <h2>What is the impact of a successful SQL injection attack?</h2>
+		<p>
+		    A successful SQL injection attack can result in unauthorized access to sensitive data, such as passwords, credit card details, or personal user information. Many high-profile data breaches in recent years have been the result of SQL injection attacks, leading to reputational damage and regulatory fines. In some cases, an attacker can obtain a persistent backdoor into an organization's systems, leading to a long-term compromise that can go unnoticed for an extended period.
+	    </p>
+		<h2>SQL injection examples</h2>
+        <p>
+            There are a wide variety of SQL injection vulnerabilities, attacks, and techniques, which arise in different situations. Some common SQL injection examples include:
+        </p>
+        <ul>
+            <li>Retrieving hidden data, where you can modify an SQL query to return additional results.</li>
+            <li>Subverting application logic, where you can change a query to interfere with the application's logic.</li>
+            <li>UNION attacks, where you can retrieve data from different database tables.</li>
+            <li>Examining the database, where you can extract information about the version and structure of the database.</li>
+            <li>Blind SQL injection, where the results of a query you control are not returned in the application's responses.</li>
+        </ul>
+        <h2>Retrieving hidden data</h2>
+        <p>
+            Consider a shopping application that displays products in different categories. When the user clicks on the Gifts category, their browser requests the URL:
+        </p>
+        <code>https://insecure-website.com/products?category=Gifts</code>
+        <p>
+            This causes the application to make an SQL query to retrieve details of the relevant products from the database:
+        </p>
+        <code>SELECT * FROM products WHERE category = 'Gifts' AND released = 1</code>
+        <p>
+            This SQL query asks the database to return:
+        </p>
+        <ul>
+            <li>
+                all details (*)
+            </li>
+            <li>
+                from the products table
+            </li>
+            <li>
+                where the category is Gifts
+            </li>
+            <li>
+                and released is 1.
+            </li>
+        </ul>
+        <p>
+            The restriction <code>released = 1</code> is being used to hide products that are not released. For unreleased products, presumably <code>released = 0</code>.
+        </p>
+        <p>
+            The application doesn't implement any defenses against SQL injection attacks, so an attacker can construct an attack like:
+        </p>
+        <code>https://insecure-website.com/products?category=Gifts'--</code>
+        <p>
+            This results in the SQL query:
+        </p>
+        <code>SELECT * FROM products WHERE category = 'Gifts'--' AND released = 1</code>
+        <p>
+            The key thing here is that the double-dash sequence <code>--</code> is a comment indicator in SQL, and means that the rest of the query is interpreted as a comment. This effectively removes the remainder of the query, so it no longer includes <code>AND released = 1</code>. This means that all products are displayed, including unreleased products.
+        </p>
+        <p>
+            Going further, an attacker can cause the application to display all the products in any category, including categories that they don't know about:
+        </p>
+        <code>https://insecure-website.com/products?category=Gifts'+OR+1=1--</code>
+        <p>
+            This results in the SQL query:
+        </p>
+        <code>SELECT * FROM products WHERE category = 'Gifts' OR 1=1--' AND released = 1</code>
+        <p>
+            The modified query will return all items where either the category is Gifts, or 1 is equal to 1. Since <code>1=1</code> is always true, the query will return all items.
+        </p>
     ");
