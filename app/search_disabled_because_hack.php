a7cd3d5cff0c85eb38d09fefba40d74af8fd09d9
John Verbiest <john@johnverbiest.be>
2022-11-08 15:41:42 +0100

    Better error handling is better

--- app/search_disabled_because_hack.php
+++ app/search_disabled_because_hack.php
@@ -29,19 +29,23 @@
 
                 $keyword = $_GET['search'];
                 $query = "SELECT id, title, excerpt FROM articles WHERE id > 0 AND article LIKE '%$keyword%'";
+                $results = $db_conn->query($query);
 
                 echo '<p>[DEBUG] Executing query: <pre><code>' . $query . '</code></pre></p>';
 
-                echo '<h2>Search results</h2>';                
-                echo '<table class="table table-striped"><th>Title</th><th>Excerpt</th>';
-                
-                $results = $db_conn->query($query);
-                while($row = $results->fetch_assoc()) {
+                if ($results == false) {
+                    echo '<div class="alert alert-danger"><h4 class="alert-heading">[DEBUG] Database did a booboo!</h4>' . mysqli_error($db_conn) . '</div>';
+                } else {
 
-                    echo "<tr><td width='25%'><a href='/article.php?id=" . $row['id'] . "'>".$row['title']."</a></td><td>".$row['excerpt']."</td></tr>";
+                    echo '<h2>Search results</h2>';
+                    echo '<table class="table table-striped"><th>Title</th><th>Excerpt</th>';
+                    while ($row = $results->fetch_assoc()) {
 
+                        echo "<tr><td width='25%'><a href='/article.php?id=" . $row['id'] . "'>" . $row['title'] . "</a></td><td>" . $row['excerpt'] . "</td></tr>";
+
+                    }
+                    echo '</table>';
                 }
-                echo '</table>';
             }
             ?>
         </div>
