3ecaca5a62138b03a67068a58e79daa3ddfd950d
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-11-08 15:11:48 +0100

    Add super duper hidden login page

--- app/includes/menu.inc.php
+++ app/includes/menu.inc.php
@@ -12,6 +12,8 @@
       <li class="nav-item">
         <a class="nav-link" href="/about.php">About</a>
       </li>
-     
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="/login.php">Login</a>
+      </li> -->
   </div>
 </nav>
