203379bc43b05a7b21ea183861e160d92fdd3b66
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-11-08 16:27:16 +0100

    Bootstrapping the login page

--- app/login.php
+++ app/login.php
@@ -31,32 +31,49 @@ if (isset($_POST['username'])) {
 </head>
 
 <body>
+    <div class="container">
 
-<?php if (!isset($_COOKIE["user_id"])) { ?>
-    <h3>Get in!</h3>
-    <form method="post">
-        <table>
-            <tr><td>Username: </td><td><input type="text" name="username" value="<?= $_POST['username']; ?>" /></td></tr>
-            <tr><td>Password: </td><td><input type="password" name="password" value="<?= $_POST['password']; ?>" /></td></tr>
-        </table>
-        <input type="submit" value="Log In!">
-    </form>
-<?php } else { ?>
+        <?php include('includes/menu.inc.php') ?>
 
-    <h3>Welcome <?=$_COOKIE["user_description"] ?></h3>
-    <?php if ($_COOKIE["user_isAdmin"] == "1") { ?>
-        <p>You made it to admin! Hooray!</p>
-        <p>You can gloat about it with using this token: H101{<?php
-            $date = date(DATE_ATOM);
-            $token = base64_encode("$date|".md5($date));
-            echo $token;
-            ?>}</p>
-    <?php } else { ?>
-        <!-- Welcome esteemed guest, Please do not tamper with cookies to get to admin! -->
-        <p>You are not admin, so there is not that much too see here.</p>
-        <p>Did you know that the cookie monster made it first appearance in 1966? We all love Cookies!</p>
+
+    <?php if (!isset($_COOKIE["user_id"])) { // user not logged in ?>
+        <h3>Get in!</h3>
+        <form method="post">
+            <table>
+                <tr><td>Username: </td><td><input type="text" name="username" value="<?= $_POST['username']; ?>" /></td></tr>
+                <tr><td>Password: </td><td><input type="password" name="password" value="<?= $_POST['password']; ?>" /></td></tr>
+            </table>
+            <input type="submit" value="Log In!">
+        </form>
+    <?php } else { // User logged in ?>
+
+        <h3>Welcome <?=$_COOKIE["user_description"] ?></h3>
+        <?php if ($_COOKIE["user_isAdmin"] == "1") { // check if user is admin ?>
+            <div class="alert alert-success">
+                <h4 class="alert-heading">You made it to admin! Hooray!</h4>
+                <p>You can gloat about it with using this token:</p>
+                <p><strong>H101{<?php
+                    $date = date(DATE_ATOM);
+                    $token = base64_encode("$date|".md5($date));
+                    echo $token;
+                    ?>}</strong></p>
+
+                <p>
+                    You can send your token to <a href="https://infinitas-chat.slack.com/archives/C049X61U4S1" target="_blank">#ilpt-jamboree-hacking-101</a> to get eternal bragging rights.
+                </p>
+                <p>
+                    <img src="/assets/img/eternal-glory.jpg" class="img-fluid" />
+                </p>
+            </div>
+        <?php } else { // regular user ?>
+            <!-- Welcome esteemed guest, Please do not tamper with cookies to get to admin! -->
+            <p>You are not admin, so there is not that much too see here.</p>
+            <p>Did you know that the cookie monster made its first appearance in 1966? We all love Cookies!</p>
+            <p>
+                <img src="/assets/img/cookiemonster.jfif" class="img-fluid" />
+            </p>
+        <?php } ?>
     <?php } ?>
-<?php } ?>
-
+    </div>
 </body>
 </html>
