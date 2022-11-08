2cf7c72e683b4af799903c0a85358a90c930ee80
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-11-08 16:37:42 +0100

    Add about page

---
+++ app/about.php
@@ -0,0 +1,16 @@
+<html lang="en">
+    <head>
+        <title>Welcome to "All Secure" - A very secure blog!</title>
+        <?php include('includes/head.inc.php') ?>
+    </head>
+
+    <body>
+        <div class="container">
+
+        <?php include('includes/menu.inc.php') ?>
+
+        <p><iframe src='https://gfycat.com/ifr/AlertCalmHerring' frameborder='0' scrolling='no' allowfullscreen width='640' height='412'></iframe></p>
+
+        </div>
+    </body>
+</html>
