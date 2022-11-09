e871213d7681f5c2bda3802af0e675e9e6da2797
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-11-09 14:43:08 +0100

    No bots for you!

--- app/includes/head.inc.php
+++ app/includes/head.inc.php
@@ -1,3 +1,4 @@
+<meta name="robots" content="noindex">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
